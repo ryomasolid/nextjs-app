7a9c11bfae2cd6ada643d0d5b1056262a5ae0d58
BTM\d-ogata <d-ogata@b-tm.co.jp>
2025-01-27 11:57:53 +0900

    fix: redirectを修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function page() {
-  redirect("/features/login");
+export default async function page() {
+  redirect('/features/login')
 }
