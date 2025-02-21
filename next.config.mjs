844685dcf761d292815f6651ec3530b994b6cb9c
Daichi <ryomasolid@yahoo.co.jp>
2025-02-22 02:35:13 +0900

    fix: next.config.js修正

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,17 @@ const nextConfig = {
   experimental: {
     appDir: true, // appディレクトリを使用している場合
   },
-  // output: 'export' を削除
+  // 環境変数を設定
+  env: {
+    MY_REGION: process.env.MY_REGION, // 環境変数を追加
+  },
+  serverRuntimeConfig: {
+    MY_ACCESS_KEY_ID: process.env.MY_ACCESS_KEY_ID, // サーバー側でのみ使用
+    MY_SECRET_ACCESS_KEY: process.env.MY_SECRET_ACCESS_KEY, // サーバー側でのみ使用
+  },
+  publicRuntimeConfig: {
+    MY_REGION: process.env.MY_REGION, // クライアント側で使用
+  },
 };
 
 export default nextConfig;
