518d7b6df47e5d517181a5bfd77c3ecf0de73466
Daichi <ryomasolid@yahoo.co.jp>
2025-02-21 19:41:50 +0900

    feat: Backup機能ページ実装

---
+++ app/features/backup/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Typography } from "@mui/material";
+
+export default function BackupPage() {
+  return (
+    <>
+      <Typography>Backup</Typography>
+      <iframe
+        src="https://www.google.com/?hl=ja"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </>
+  );
+}
