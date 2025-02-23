95689eeed418077f80c8398bcef32e24d0024063
Daichi <ryomasolid@yahoo.co.jp>
2025-02-23 23:23:32 +0900

    fix: リファクタリング

--- app/features/home/page.tsx
+++ app/features/home/page.tsx
@@ -1,14 +1,12 @@
-"use client";
+'use client';
 
-import { Box, Tab, Tabs } from "@mui/material";
-import { useState } from "react";
-import ChatgptPage from "../chatgpt/page";
-import GooglePage from "../google/page";
-import HonyakuPage from "../honyaku/page";
-import BackupPage from "../backup/page";
+import { Box, Tab, Tabs } from '@mui/material';
+import { useState } from 'react';
+import HonyakuPage from '../honyaku/page';
+import BackupPage from '../backup/page';
 
 export default function HomePage() {
-  const [currentTab, setCurrentTab] = useState<string>("1");
+  const [currentTab, setCurrentTab] = useState<string>('1');
 
   const handleChange = (e: React.SyntheticEvent, value: string) => {
     setCurrentTab(value);
@@ -19,18 +17,14 @@ export default function HomePage() {
         <Box>
           <Tabs value={currentTab} onChange={handleChange} centered>
             <Tab label="Home" value="1" />
-            <Tab label="Google" value="2" />
-            <Tab label="ChatGPT" value="3" />
-            <Tab label="Honyaku" value="4" />
-            <Tab label="Backup" value="5" />
+            <Tab label="Honyaku" value="2" />
+            <Tab label="Backup" value="3" />
           </Tabs>
         </Box>
-        <Box sx={{ height: "50vh" }}>
-          {currentTab === "1" && <Box>Home</Box>}
-          {currentTab === "2" && <GooglePage />}
-          {currentTab === "3" && <ChatgptPage />}
-          {currentTab === "4" && <HonyakuPage />}
-          {currentTab === "5" && <BackupPage />}
+        <Box sx={{ height: '50vh' }}>
+          {currentTab === '1' && <Box>Home</Box>}
+          {currentTab === '2' && <HonyakuPage />}
+          {currentTab === '3' && <BackupPage />}
         </Box>
       </Box>
     </>
